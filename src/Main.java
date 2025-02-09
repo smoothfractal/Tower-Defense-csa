fedb25b28b0c7eb2076c8d5f61feb88f3e00b9c2
smoothfractal <asta.an00@gmail.com>
2025-02-09 13:51:55 -0600

    some basics + extras from tutorial

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Tower Defense");
+
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel); // add gamePanel to the window
+        window.pack(); // causes this Window to be sized to fit the preferred size and layouts of its subcomponents(gamePanel)
+
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        gamePanel.startGameThread();
+
 
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,10 @@
+
+public class Player extends Entity {
+    GamePanel gp;
+    KeyHandler keyH;
+    public Player(GamePanel gp, KeyHandler keyH){
+        this.gp = gp;
+        this.keyH = keyH;
+    }
+
+}
